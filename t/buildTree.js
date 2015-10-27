b456494f1934aefeee5ed367c96e389d90d5fcc7
Peter <Peter@users.noreply.github.com>
2015-10-28 03:45:47 +0800

    'aaa'

--- t/buildTree.js
+++ t/buildTree.js
@@ -1,6 +1,92 @@
 var parseString = require('./parseString.js');
 var utils = require('./utils.js');
+var _util = require('util');
 module.exports = buildTree;
+
+function Node(opts) {
+	this.textContent = '';
+	this.nodeType = 0;
+	this.parentNode = null;
+	this.children = null;
+}
+
+// 1	Element	代表元素	Element, Text, Comment, ProcessingInstruction, CDATASection, EntityReference
+// 2	Attr	代表属性	Text, EntityReference
+// 3	Text	代表元素或属性中的文本内容。	None
+// 4	CDATASection	代表文档中的 CDATA 部分（不会由解析器解析的文本）。	None
+// 5	EntityReference	代表实体引用。	Element, ProcessingInstruction, Comment, Text, CDATASection, EntityReference
+// 6	Entity	代表实体。	Element, ProcessingInstruction, Comment, Text, CDATASection, EntityReference
+// 7	ProcessingInstruction	代表处理指令。	None
+// 8	Comment	代表注释。	None
+// 9	Document	代表整个文档（DOM 树的根节点）。	Element, ProcessingInstruction, Comment, DocumentType
+// 10	DocumentType	向为文档定义的实体提供接口	None
+// 11	DocumentFragment	代表轻量级的 Document 对象，能够容纳文档的某个部分	Element, ProcessingInstruction, Comment, Text, CDATASection, EntityReference
+// 12	Notation	代表 DTD 中声明的符号。	None
+Node.ELEMENT_NODE = 1;
+Node.ATTRIBUTE_NODE = 2;
+Node.TEXT_NODE = 3;
+Node.CDATA_SECTION_NODE = 4;
+Node.ENTITY_REFERENCE_NODE = 5;
+Node.ENTITY_NODE = 6;
+Node.PROCESSING_INSTRUCTION_NODE = 7;
+Node.COMMENT_NODE = 8;
+Node.DOCUMENT_NODE = 9;
+Node.DOCUMENT_TYPE_NODE = 10;
+Node.DOCUMENT_FRAGMENT_NODE = 11;
+Node.NOTATION_NODE = 12;
+
+Node.createElement = function (tagName) {};
+Node.createTextNode = function (str) {};
+
+Node.prototype.helloWorld = function () {
+	console.log('helloWorld, I am: ' + this.tagName + ', attr: ' + this._attr);
+};
+
+function ElementNode(opts) {
+	Node.call(this, opts);
+
+	this.istackEnd = opts.istackEnd || null;
+	this.tagName = opts.tagName || '';
+	this.parentIndex = opts.parentIndex || false;
+	this.tagString = opts.tagString || '';
+
+	this.istackStart = opts.istackStart || null;
+	this._attr = opts._attr || '';
+	this.single = opts.single || false;
+	this.depth = opts.depth || 0;
+
+	this.textContent = opts.textContent || '';
+	this.attributes = opts.attributes || {};
+	this.children = opts.children || [];
+	this.parentNode = opts.parentNode || null;
+	this.nodeType = Node.ELEMENT_NODE;
+
+	this.innerHTML = '';
+	this.nodeName = '';
+	this.nodeValue = '';
+	this.style = {};
+}
+
+_util.inherits(ElementNode, Node);
+ElementNode.prototype.addEventListener = function () {};
+ElementNode.prototype.removeEventListener = function () {};
+ElementNode.prototype.querySelector = function () {};
+ElementNode.prototype.querySelectorAll = function () {};
+ElementNode.prototype.contains = function () {};
+ElementNode.prototype.matches = function () {};
+ElementNode.prototype.replaceChild = function () {};
+ElementNode.prototype.insertBefore = function () {};
+ElementNode.prototype.appendChild = function () {};
+ElementNode.prototype.removeChild = function () {};
+ElementNode.prototype.getElementsByTagName = function () {};
+ElementNode.prototype.createAttribute = function () {};
+ElementNode.prototype.getAttribute = function () {};
+ElementNode.prototype.setAttribute = function () {};
+ElementNode.prototype.removeAttribute = function () {};
+
+// DocumentElementNode.prototype.createElement = function () {};
+// DocumentElementNode.prototype.createTextNode = function () {};
+// DocumentElementNode.prototype.getElementById = function () {};
 
 function setSingleNode(node, map, tag, match) {
 	node.single = true;
@@ -21,44 +107,18 @@ function setNode(node, map, startTag, stack) {
 	map[node.istackStart] = node;
 }
 
-function getInitNode(opts) {
-	return {
-		istackEnd : opts.istackEnd,
-		tagName : opts.tagName,
-		parentIndex : opts.parentIndex,
-		tagString : opts.tagString,
-		//
-		istackStart : null,
-		textContent : '',
-		_attr : '',
-		attributes : null,
-		single : false,
-		nodeType : 1,
-		parentNode : null,
-		children : [],
-		depth : 0
-	};
+function getNewElementNode(opts) {
+	return new ElementNode(opts);
 }
 
 function getStack(str) {
-	return typeof str === 'string'
-		? parseString(str)
-		: Array.isArray(str) && str.tagNames
-		? str
-		: null;
+	return typeof str === 'string' ? parseString(str) :
+		Array.isArray(str) && str.tagNames ? str :
+		null;
 }
 
-function buildTree(str) {
-	var stack = getStack(str);
-	if (!stack)
-		throw new Error('buildTree: not standard input arg');
-
-	var tracker = [];
-	var parentIndex = false;
-	var map = {};
-
-	//the tree
-	var tree = stack.reduce(function (memo, tag, index) {
+function buildStructure(stack, tracker, parentIndex, map) {
+	return function (memo, tag, index) {
 		if (tag[0] !== '<' || !utils.rtag.test(tag))
 			return memo;
 
@@ -67,7 +127,7 @@ function buildTree(str) {
 		if (!match || !tagName)
 			throw new Error('buildTree: miss match tag: ' + tag);
 
-		var node = getInitNode({
+		var node = getNewElementNode({
 			istackEnd: index,
 			tagName: tagName,
 			parentIndex: parentIndex,
@@ -100,27 +160,39 @@ function buildTree(str) {
 			throw new Error('buildTree: ' + endTag.tagName + ' !== ' + startTag.tagName);
 
 		setNode(node, map, startTag, stack);
-
 		parentIndex = startTag.parentIndex;
 		memo.push(node);
 		return memo;
-	}, [])
-	.sort(function (a, b) {
-		return a.istackStart - b.istackStart;
-	})
-	.map(function (node, index) {
+	};
+}
+
+function sortNodesOrder(a, b) {
+	return a.istackStart - b.istackStart;
+}
+
+function setNodesRelation(map) {
+	return function (node, index) {
 		if (index > 0) {
 			node.parentNode = map[node.parentIndex];
 			node.parentNode.children.push(node);
 			node.depth = node.parentNode.depth + 1;
 		}
 		return node;
-	});
+	};
+}
 
-	if (tracker.length) {
-		console.log(tracker);
-		throw new Error('buildTree: tracker not empty');
-	}
+function buildTree(str) {
+	var stack = getStack(str);
+	if (!stack)
+		throw new Error('buildTree: not standard input arg');
+
+	var tracker = [], parentIndex = false, map = {},
+		tree = stack.reduce(buildStructure(stack, tracker, parentIndex, map), [])
+			.sort(sortNodesOrder)
+			.map(setNodesRelation(map));
+
+	if (tracker.length)
+		throw new Error('buildTree: tracker not empty: ' + tracker.join(''));
 
 	tree.stack = stack;
 	tree.indexMap = map;
@@ -128,3 +200,8 @@ function buildTree(str) {
 
 	return tree;
 }
+
+// var str = '<div> <ul id="ul-1"> asas </ul> </div>';
+// var tree = buildTree(str);
+// console.log(tree);
+// tree[1].helloWorld();

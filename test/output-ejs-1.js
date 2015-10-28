a68ea2843519da826a390a0701dddb3843fbf65c
Peter <Peter@users.noreply.github.com>
2015-10-28 20:58:58 +0800

    'aaa'

--- test/output-ejs-1.js
+++
@@ -1,7 +0,0 @@
-define(function (require) {
-	return '' +
-		'<div class=\'container\'>' +
-		'	<h4 class=\'edu-title\'><%= x %></h4>' +
-		'	<p>' + require("./output-ejs-2") + '</p>' +
-		'</div>';
-});
--- test/output-ejs-2.js
+++
@@ -1,6 +0,0 @@
-define(function (require) {
-	return '' +
-		'<div class=\'ejs-2\'>' +
-		'	<h4 class=\'edu-title\'><%= y %></h4>' +
-		'</div>';
-});
